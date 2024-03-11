859a5837f335dd5633671a8daeefb5fa685cc4c8
Sumanth9498 <137343783+Sumanth9498@users.noreply.github.com>
2024-03-10 19:55:46 -0500

    Update root_elements.sql

--- models/marts/core/root_elements.sql
+++ models/marts/core/root_elements.sql
@@ -1,1 +1,16 @@
+{{ config(materialized='table') }}
 
+SELECT
+  PARSE_JSON(data):id AS id,
+  PARSE_JSON(data):startTime AS startTime,
+  PARSE_JSON(data):messageTime AS messageTime,
+  PARSE_JSON(data):sportType AS sportType,
+  PARSE_JSON(data):matchState AS matchState,
+  PARSE_JSON(data):status AS status,
+  PARSE_JSON(data):marketCount AS marketCount,
+  PARSE_JSON(data):eventType AS eventType,
+  PARSE_JSON(data):updatesCount AS updatesCount,
+  PARSE_JSON(data):displayInfo AS displayInfo,
+  PARSE_JSON(data):lastModifiedTime AS lastModifiedTime
+FROM
+  {{ ref('raw_tipico_events') }}
